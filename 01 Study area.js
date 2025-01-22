05ab06f26d1260e76703fcdab7b57dfc12992ebf
Chuanwu Zhao (赵传武) <151167977+Charm-ghao@users.noreply.github.com>
2025-01-23 01:25:37 +0800

    Update 01 Study area.js

--- 01 Study area.js
+++ 01 Study area.js
@@ -4,8 +4,43 @@ In this study, we selected 14 study sites.
 */
 
 // Site 1
-var NF = 
-
+var NF = ee.Geometry.Point([51.56, 25.32])
 
 //// Site 2
-// var NF = 
+// var NF = ee.Geometry.Point([144.95, -37.82])
+
+//// Site 3
+// var NF = ee.Geometry.Point([2.26, 48.89])
+
+//// Site 4
+// var NF = ee.Geometry.Point([114.39, 30.66])
+
+//// Site 5
+// var NF = ee.Geometry.Point([-71.11, -36.87])
+
+//// Site 6
+// var NF = ee.Geometry.Point([-122.44, 49.91])
+
+//// Site 7
+// var NF = ee.Geometry.Point([46.53, 42.88])
+
+//// Site 8
+// var NF = ee.Geometry.Point([19.34, -33.74])
+
+//// Site 9
+// var NF = ee.Geometry.Point([-60.39, -5.14])
+
+//// Site 10
+// var NF = ee.Geometry.Point([38.67, 7.72])
+
+//// Site 11
+// var NF = ee.Geometry.Point([-71.61, 10.73])
+
+//// Site 12
+// var NF = ee.Geometry.Point([121.03, 32.73])
+
+//// Site 13
+// var NF = ee.Geometry.Point([116.48, 28.96])
+
+//// Site 14
+// var NF = ee.Geometry.Point([82.67, 41.01])
